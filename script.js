19038a9a276c4e9cfc5fef046f3e7b4aeb92613b
shivar007 <mr.adarsh2709@gmail.com>
2025-01-22 20:19:01 +0530

    added index.html style.css and script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function changeGreeting() {
     const nameElement = document.getElementById("name");
     const wishNote = document.querySelector(".wish-note");
     
-    nameElement.textContent = "Adee"; // Change "John Doe" to the actual birthday person's name.
-    wishNote.textContent = "May all your dreams come true, and may this year be the best one yet!";
+    nameElement.textContent = "Abhishree"; // Change "John Doe" to the actual birthday person's name.
+    wishNote.textContent = "Happy Birthday, Abhishree! 🎉🌸 On your special day, may your heart be filled with joy, your soul with peace, and your year with endless happiness and success. You deserve all the love and laughter in the world—today and always. Have an amazing birthday celebration! 🎂✨💖!";
 }
